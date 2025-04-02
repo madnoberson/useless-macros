8977aa802c4f0703b67043cb96937e8e1338a147
Madnoberson <baseddepartmentzx77@gmail.com>
2025-04-02 11:05:21 +0400

    Add basic scenario tests for `add_setters` macro

---
+++ crates/add_setters/tests/integration.rs
@@ -0,0 +1,44 @@
+use std::fmt::Debug;
+
+use useless_add_setters::add_setters;
+
+#[test]
+fn test_basic_scenario() {
+    #[add_setters]
+    #[derive(Debug, PartialEq, Default)]
+    struct Foo {
+        bar: u16,
+        baz: String,
+    }
+
+    let foo = Foo::default().with_bar(100 as u16).with_baz("some_text");
+    let expected_foo = Foo {
+        bar: 100 as u16,
+        baz: String::from("some_text"),
+    };
+
+    assert_eq!(foo, expected_foo)
+}
+
+#[test]
+fn test_basic_scenarion_with_generics() {
+    #[add_setters]
+    #[derive(Debug, PartialEq, Default)]
+    struct Foo<T>
+    where
+        T: Debug + PartialEq + Default,
+    {
+        bar: u16,
+        baz: T,
+    }
+
+    let foo = Foo::<String>::default()
+        .with_bar(100 as u16)
+        .with_baz("some_text");
+    let expected_foo = Foo {
+        bar: 100 as u16,
+        baz: String::from("some_text"),
+    };
+
+    assert_eq!(foo, expected_foo)
+}
